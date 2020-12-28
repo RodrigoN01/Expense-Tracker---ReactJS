4ed6f1ada7add329cc8b19a94bc5575fe158e32f
Rodrigo Novaes <rod_novaes@hotmail.com>
2020-12-28 17:39:00 +1300

    final commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SpeechProvider } from '@speechly/react-client';
 
+import { Provider } from './context/context';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <SpeechProvider appId='c1653e6b-1edb-4966-bece-df0cd0c4d291' language='en-US'>
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
+  document.getElementById('root')
+);
